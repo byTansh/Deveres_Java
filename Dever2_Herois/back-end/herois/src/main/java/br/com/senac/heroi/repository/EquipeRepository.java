a1b3f5db6cdf758fb147511b8dfce3b4f3d68c40
Tiago moura <150278374+byTansh@users.noreply.github.com>
2024-11-25 10:38:59 -0300

    Delete Dever2_Herois directory

--- Dever2_Herois/back-end/herois/src/main/java/br/com/senac/heroi/repository/EquipeRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.senac.heroi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.senac.heroi.model.Equipe;
-
-public interface EquipeRepository extends JpaRepository<Equipe, Integer> {
-
-}
